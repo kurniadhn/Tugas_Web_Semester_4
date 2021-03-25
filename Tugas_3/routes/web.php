653a2e4a2590ceb9957d03779759721fd25649c0
kurniadhan <dickynakiri@gmail.com>
2021-03-25 08:24:13 +0700

    Mengupdate file pada BKPM Acara 10

--- Tugas_3/routes/web.php
+++ Tugas_3/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+// use App\Http\Controllers\ManagementUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('user', [ManagementUserController::class, 'index']);
+Route::resource('user', 'ManagementUserController');
