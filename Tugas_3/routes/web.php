081ec24264011fae10481867667821367e8319f0
kurniadhan <dickynakiri@gmail.com>
2021-03-25 08:32:35 +0700

    Mengupdate Route Web

--- Tugas_3/routes/web.php
+++ Tugas_3/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\ManagementUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('user', [ManagementUserController::class, 'index']);
 Route::resource('user', 'ManagementUserController');
